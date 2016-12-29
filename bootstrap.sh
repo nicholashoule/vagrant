93183e36e067a77aa9f3f05fbd6754e6d9b7b740
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-12-29 15:48:07 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # bootstrap file for configuring system
+printf "%s\n" "[BOOTSTRAP]: running"
+
+# Update system 
 # sudo zypper update -y
 
+# Update VBox Guest Addintions
 # cd /opt/VBoxGuestAdditions-*/init
 # sudo ./vboxadd setup
-
